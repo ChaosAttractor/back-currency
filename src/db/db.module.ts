4cef1caa83f6cc51e2f03f0abcc585781afee42e
surf punk <povy.901@gmail.com>
2023-05-29 18:22:39 +0300

    init

---
+++ src/db/db.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { Pool } from 'pg';
+import { PG_CONNECTION } from '../constants';
+import * as process from 'process';
+
+const dbProvider = {
+  provide: PG_CONNECTION,
+  useValue: new Pool({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DB,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  }),
+};
+
+@Module({
+  providers: [dbProvider],
+  exports: [dbProvider],
+})
+export class DbModule {}
