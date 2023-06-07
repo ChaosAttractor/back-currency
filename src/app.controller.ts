c3ff80219f994f0dbf091be89bc85be2fa670dcb
surf punk <povy.901@gmail.com>
2023-06-07 14:08:19 +0300

    типизация GET запроса

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Valute } from './models/Valute.model';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('currency')
-  getCurrency(): Promise<unknown[]> {
+  getCurrency(): Promise<Valute[]> {
     return this.appService.getCurrency();
   }
 }
